a83f4197af3ef4084e504e77d13e6a754f780740
megphillips21 <megphillips21@gmail.com>
2021-12-05 18:01:36 -0800

    added function to write file, and initialize app

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMD = require(`utils\generateMarkdown.js`);
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, error => {
+        if (error) {
+            console.log(error);
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(answers => {
+        // generateMD(answers);
+        writeToFile(`readme.MD`, generateMD(answers));
+        });
+    }
+
 
 // Function call to initialize app
 init();
